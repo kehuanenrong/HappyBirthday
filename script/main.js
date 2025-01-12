380362814140dcae3e2a25b81e3c13adaae4e761
kehuanenrong <1983583653@qq.com>
2025-01-12 12:02:10 +0800

    添加了背景音乐播放按钮

--- script/main.js
+++ script/main.js
@@ -1,19 +1,19 @@
 // 在文档加载时预加载音频
-let audio = null
-document.addEventListener("DOMContentLoaded", () => {
-  audio = new Audio("../audio/bg.mp3")
-  audio.preload = "auto"
-})
+// let audio = null
+// document.addEventListener("DOMContentLoaded", () => {
+//   audio = new Audio("../audio/bg.mp3")
+//   audio.preload = "auto"
+// })
 
-const startButton = document.getElementById('startButton')
-startButton.addEventListener('click', () => {
-  audio.loop = true;
-  audio.autoplay = true;
-  audio.preload = 'auto';
-  audio.play().catch(function (error) {
-    console.log("Autoplay failed:", error);
-  });
-})
+// const startButton = document.getElementById('startButton')
+// startButton.addEventListener('click', () => {
+//   audio.loop = true;
+//   audio.autoplay = true;
+//   audio.preload = 'auto';
+//   audio.play().catch(function (error) {
+//     console.log("Autoplay failed:", error);
+//   });
+// })
 
 // 添加页面加载后自动播放音频文件的功能
 // console.log(document.getElementById('background-music'));
@@ -75,6 +75,31 @@ const fetchData = () => {
       });
     });
 };
+
+let audio = null
+
+// 在文档加载时预加载音频
+document.addEventListener("DOMContentLoaded", () => {
+  audio = new Audio("../audio/bg.mp3")
+  audio.preload = "auto"
+})
+
+const playPauseButton = document.getElementById('playPauseButton')
+let isPlaying = false // 初始状态为未播放
+
+playPauseButton.addEventListener('click', () => {
+  isPlaying = !isPlaying // 切换播放状态
+
+  if (isPlaying) {
+    // 如果当前是播放状态，则开始播放音频并更新按钮样式
+    audio.play()
+    playPauseButton.classList.add('playing')
+  } else {
+    // 如果当前是暂停状态，则暂停音频并更新按钮样式
+    audio.pause()
+    playPauseButton.classList.remove('playing')
+  }
+})
 
 // Animation Timeline
 const animationTimeline = () => {
